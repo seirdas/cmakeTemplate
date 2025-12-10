547a1469e3bc6bb8377c25caf8d4a8c2b7845799
Sergio Esteban Crespo <secrespo@indra.es>
2025-12-10 20:31:09 +0100

    msvc como toolchain de cmakepreset. añadido icono en el exe

---
+++ resources/resources.h
@@ -0,0 +1,3 @@
+// resource.h
+
+#define IDI_ICON1 101  // Define un identificador para usar en tu código
---
+++ toolchains/msvc.cmake
@@ -0,0 +1,16 @@
+	# Configuraciones específicas para Visual Studio
+    
+    # Especifica el proyecto principal
+	set_property(DIRECTORY PROPERTY VS_STARTUP_PROJECT ${PROJECT_NAME})
+	# Allow for using folders
+	set_property(GLOBAL PROPERTY USE_FOLDERS ON)
+	# Set runtime library to Multi-threaded DLL for MSVC
+    set_property(TARGET ${PROJECT_NAME} PROPERTY
+        MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>DLL"
+    )
+    set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>DLL")
+    # Agrupar archivos de cabecera
+    source_group("Include" FILES ${HEADERS})
+    # Agrupar archivos fuente
+    source_group("Source" FILES ${SOURCES})
+    source_group("Resources" FILES ${RESOURCES})
