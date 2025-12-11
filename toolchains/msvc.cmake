f974eb3461f797b59a7c4280877a47ca93c24465
Sergio Esteban Crespo <secrespo@indra.es>
2025-12-11 19:27:07 +0100

    limpia. añado README (vacio)

--- toolchains/msvc.cmake
+++ toolchains/msvc.cmake
@@ -1,16 +1,21 @@
-	# Configuraciones específicas para Visual Studio
-    
+	#
+    # Configuraciones específicas para Visual Studio
+    #
+
     # Especifica el proyecto principal
 	set_property(DIRECTORY PROPERTY VS_STARTUP_PROJECT ${PROJECT_NAME})
+
 	# Allow for using folders
 	set_property(GLOBAL PROPERTY USE_FOLDERS ON)
+
 	# Set runtime library to Multi-threaded DLL for MSVC
     set_property(TARGET ${PROJECT_NAME} PROPERTY
         MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>DLL"
     )
     set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>DLL")
-    # Agrupar archivos de cabecera
-    source_group("Include" FILES ${HEADERS})
-    # Agrupar archivos fuente
-    source_group("Source" FILES ${SOURCES})
+    
+    # Agrupar archivos de cabecera, fuente y recursos en carpetas
+    source_group("Include"  FILES ${HEADERS})
+    source_group("Source"   FILES ${SOURCES})
     source_group("Resources" FILES ${RESOURCES})
+    
