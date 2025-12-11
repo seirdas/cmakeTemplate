29f486a02dc8f1862acd8c72bd420ef9a76034a6
Sergio Esteban Crespo <secrespo@indra.es>
2025-12-11 20:16:49 +0100

    CMAKE_SYSTEM_NAME en el toolchain (?)

--- toolchains/msvc.cmake
+++ toolchains/msvc.cmake
@@ -1,21 +1,22 @@
-	#
-    # Configuraciones específicas para Visual Studio
-    #
 
-    # Especifica el proyecto principal
-	set_property(DIRECTORY PROPERTY VS_STARTUP_PROJECT ${PROJECT_NAME})
+# Toolchain para MSVC de Visual Studio
 
-	# Allow for using folders
-	set_property(GLOBAL PROPERTY USE_FOLDERS ON)
+set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_PROCESSOR x86_64)
 
-	# Set runtime library to Multi-threaded DLL for MSVC
-    set_property(TARGET ${PROJECT_NAME} PROPERTY
-        MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>DLL"
-    )
-    set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>DLL")
-    
-    # Agrupar archivos de cabecera, fuente y recursos en carpetas
-    source_group("Include"  FILES ${HEADERS})
-    source_group("Source"   FILES ${SOURCES})
-    source_group("Resources" FILES ${RESOURCES})
-    
+# Especifica el proyecto principal
+set_property(DIRECTORY PROPERTY VS_STARTUP_PROJECT ${PROJECT_NAME})
+
+# Allow for using folders
+set_property(GLOBAL PROPERTY USE_FOLDERS ON)
+
+# Set runtime library to Multi-threaded DLL for MSVC
+set_property(TARGET ${PROJECT_NAME} PROPERTY
+    MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>DLL"
+)
+set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>DLL")
+
+# Agrupar archivos de cabecera, fuente y recursos en carpetas
+source_group("Include"  FILES ${HEADERS})
+source_group("Source"   FILES ${SOURCES})
+source_group("Resources" FILES ${RESOURCES})
