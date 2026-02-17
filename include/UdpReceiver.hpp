a0d3ff5b527ff5b49a1da4f4693f81d5fd415af0
Sergio <xergio.e@hotmail.com>
2026-02-18 00:05:03 +0100

    ajustes constructor y otros

--- include/UdpReceiver.hpp
+++ include/UdpReceiver.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <asio.hpp>
 #include <thread>
 #include <atomic>
@@ -6,15 +8,26 @@
 #include <condition_variable>
 #include <vector>
 
-
+/**
+ * @brief Gestiona la recepción de datos UDP de forma asíncrona. Permite configurar la IP local, el puerto y el tamaño máximo de los paquetes.
+ *          Los datos recibidos se almacenan en una cola compartida, accesible mediante métodos de push/pop. 
+ *          El receptor se ejecuta en un hilo separado para no bloquear el hilo principal.
+ */
 class UdpReceiver {
+
 public:
-    UdpReceiver(const std::string& ipLocal = "", short port, unsigned int packet_size) 
+
+    /**
+     * @brief Constructor de UdpReceiver. Configura el socket UDP para recibir datos en la IP y puerto especificados.
+     *        Si la IP local es vacía, se enlaza a todas las interfaces disponibles
+     * @param ipLocal - Dirección IP local a la que se desea enlazar el socket. Si es vacía, se enlaza a todas las interfaces disponibles.
+     * @param port - Puerto en el que se desea recibir los datos UDP.
+     * @param packet_size - Tamaño máximo de los paquetes UDP que se esperan recibir. Elimina el paquete si es diferente a este tamaño. Si es 0, se aceptan paquetes de cualquier tamaño.
+     */
+    UdpReceiver(const std::string& ipLocal = "", short port, unsigned int packet_size = 0) 
         : socket_(io_context_, asio::ip::udp::endpoint(asio::ip::udp::v4(), port))
         {
             is_running_ = false;
-            
-            asio::error_code ec;
             asio::ip::udp::endpoint endpoint;
             
             // Si la ip local es vacía, se enlaza a todas las interfaces
@@ -34,14 +47,28 @@ public:
                 if (ec) throw std::runtime_error("Error binding socket: " + ec.message());
             }
             socket_ = asio::ip::udp::socket(io_context_, endpoint);
-
         }
+
+
+    //Primero:
+    //Registras que quieres recibir datos.
+    //Arrancas el hilo que ejecuta el loop.
+
+    //Cuando llegan datos:
+    //El SO notifica
+    //io_context despierta
+    //Ejecuta tu lambda
 
     void start() {
         if (is_running_) return;
         is_running_ = true;
         start_receive();
-        worker_thread_ = std::thread([this]() { io_context_.run(); });
+
+        // Iniciar el hilo de trabajo para procesar las operaciones asíncronas
+        worker_thread_ = std::thread([this]() { 
+            // El hilo se queda ejecutando el io_context, procesando eventos del async_receive_from
+            io_context_.run(); 
+        });
     }
 
     void stop() {
@@ -52,17 +79,39 @@ public:
 
 private:
     void start_receive() {
+
+        // Cuando llega un paquete, se ejecuta esta lambda que maneja la recepción de datos
         socket_.async_receive_from(
             asio::buffer(recv_buffer_), remote_endpoint_,
             [this](std::error_code ec, std::size_t bytes_recvd) {
-                if (!ec && is_running_) {
-                    // Pasamos los bytes a la cola compartida
-                    std::vector<char> data(recv_buffer_.begin(), recv_buffer_.begin() + bytes_recvd);
-                    queue_.push(std::move(data));
-                    
-                    start_receive(); 
-                }
-            });
+            handle_receive(ec, bytes_recvd);
+        }
+        );
+    }
+
+    void handle_receive(std::error_code ec, std::size_t bytes_recvd) {
+        // Si el programa se está cerrando, ignoramos
+        if (!is_running_)
+            return;
+
+        // Si la operación fue cancelada (por stop())
+        if (ec == asio::error::operation_aborted)
+            return;
+
+        if (!ec) {
+            std::vector<char> data(
+                recv_buffer_.begin(),
+                recv_buffer_.begin() + bytes_recvd
+            );
+
+            queue_.push(std::move(data));
+
+            // Volvemos a esperar más datos
+            
+        } else {
+            // Opcional: loggear error real
+            std::cerr << "Receive error: " << ec.message() << "\n";
+        }
     }
 
     asio::io_context io_context_;       // Contexto de E/S para operaciones asíncronas
@@ -72,6 +121,8 @@ private:
     std::array<char, 2048> recv_buffer_;
     std::thread worker_thread_;
     std::atomic<bool> is_running_;
+
+    asio::error_code ec;
 
 
 // Gestión de cola de datos
