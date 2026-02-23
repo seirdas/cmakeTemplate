fab432e97c1f260e829d27c7c3668c7281f9c755
seirdas <xergio.e@hotmail.com>
2026-02-23 21:56:27 +0100

    Comentarios IAppControl

--- include/IAppControl.hpp
+++ include/IAppControl.hpp
@@ -7,7 +7,7 @@
  * @details Permite a los miembros de la aplicación comunicarse entre sí sin acoplarse directamente, facilitando la modularidad y el mantenimiento.
  *          Cada miembro puede implementar esta interfaz para exponer métodos que otros miembros pueden llamar, sin necesidad de conocer la implementación concreta de cada miembro.
  * @note Los constructores de los miembros deben recibir un puntero a esta interfaz para poder registrarse y comunicarse con otros miembros.
- *       Por ejemplo, MyClass(IAppControl* controller) 
+ *       Por ejemplo, MyClass(IAppControl* controller), con #include "IAppControl.hpp". En el controlador se declaran con (this) y 
  * @note Esta clase actúa como plantilla de funciones (interfaz) que pueden llamarse desde los miembros,
  *       y deberían estar definidas override en la clase principal AppController que extiende esta interfaz.
  */
@@ -16,7 +16,6 @@ public:
 
     /**
      * @brief Destructor virtual para asegurar la correcta destrucción de objetos derivados.
-     * @details Al declarar un destructor virtual, se garantiza que al eliminar un objeto a través de
      */
     virtual ~IAppControl() = default;
 
@@ -27,8 +26,7 @@ public:
     
     /**
      * @brief Método ejemplo para pedir información a uno de los miembros. 
-     * @return El puerto en el que el receptor UDP está escuchando. 
-     * @note const es para que no cambie el valor
+     * @return El puerto en el que el receptor UDP está escuchando.
      */
     virtual int get_SocketPort() const noexcept = 0;
 
