28463b9f7324627ceed86f3cc45343b8e452de17
Sergio <xergio.e@hotmail.com>
2026-03-03 22:51:25 +0100

    +lib STB para imágenes

---
+++ toolchains/lib-stb.cmake
@@ -0,0 +1,28 @@
+# -------------------------------
+# Librería de stb con dependencias
+# genera la librería stb-lib
+# -------------------------------
+
+include(FetchContent)
+
+message(STATUS "Fetching stb library...")
+
+# Usa la librería ya descargada en external/ si existe
+if (EXISTS "${EXTERNAL_LIB_PATH}/stb_src/.github")
+  message(STATUS "Using local stb source")
+  set(FETCHCONTENT_SOURCE_DIR_STB
+      "${EXTERNAL_LIB_PATH}/stb_src"
+      CACHE PATH "" FORCE)
+endif()
+
+# Declarar dependencia externa
+FetchContent_Declare(
+    stb
+    GIT_REPOSITORY https://github.com/chriskohlhoff/stb.git
+    GIT_TAG stb-1-36-0
+    GIT_SHALLOW    TRUE        # habilita --depth 1
+    SOURCE_DIR     "${EXTERNAL_LIB_PATH}/stb_src"
+    EXCLUDE_FROM_ALL TRUE
+)
+# Hacerla disponible
+FetchContent_MakeAvailable(stb)
