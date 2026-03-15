701cea7c4b3043a88cbc6800cfd89ac6bf841d43
seirdas <xergio.e@hotmail.com>
2026-03-15 14:32:22 +0100

    json toolchain con ruta +src

--- toolchains/lib-json.cmake
+++ toolchains/lib-json.cmake
@@ -4,14 +4,14 @@
 
 # Descarga el archivo json.hpp del último commit de github (si no existe)
 # Se podría hacer también con el FetchContent pero tarda más
-file(MAKE_DIRECTORY ${EXTERNAL_LIB_PATH}/nlohmann)
-if (NOT EXISTS ${EXTERNAL_LIB_PATH}/nlohmann/json.hpp)
-  message(STATUS "Downloading lastest release of nlohmann/json.hpp...")
+file(MAKE_DIRECTORY ${EXTERNAL_LIB_PATH}/nlohmann_src)
+if (NOT EXISTS ${EXTERNAL_LIB_PATH}/nlohmann_src/json.hpp)
+  message(STATUS "Downloading latest release of nlohmann/json.hpp...")
 
   # Header-only: solo se necesita el header
   file(DOWNLOAD 
     "https://github.com/nlohmann/json/releases/latest/download/json.hpp"
-    "${EXTERNAL_LIB_PATH}/nlohmann/json.hpp"
+    "${EXTERNAL_LIB_PATH}/nlohmann_src/json.hpp"
     STATUS DOWNLOAD_STATUS
   )
   if(NOT DOWNLOAD_STATUS EQUAL 0)
@@ -25,5 +25,5 @@ endif()
 # Crear una librería de interfaz para nlohmann/json
 add_library(nlohmann_json INTERFACE)
 target_include_directories(nlohmann_json INTERFACE
-  ${EXTERNAL_LIB_PATH}/nlohmann
+  ${EXTERNAL_LIB_PATH}/nlohmann_src
 )
