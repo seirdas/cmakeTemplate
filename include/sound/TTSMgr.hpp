990447f4a20471f87b179517bf7124e722eaf63e
Sergio Esteban Crespo <secrespo@indra.es>
2026-04-10 13:32:38 +0200

    TTS: fix carga de voces

--- include/sound/TTSMgr.hpp
+++ include/sound/TTSMgr.hpp
@@ -37,21 +37,23 @@ public:
             std::cout << "[TTSMgr]  Configuring model..." << std::endl;
 
             // Genera la configuración a partir de los nombres habituales de los archivos
-            std::string onnx = (models_path_+"/vits-piper-"+voicename+"/"+voicename+".onnx");
+            std::string onnx    = (models_path_+"/vits-piper-"+voicename+"/"+voicename+".onnx");
+            std::string tokens  = (models_path_+"/vits-piper-"+voicename+"/tokens.txt");
+            std::string espeak  = (models_path_+"/vits-piper-"+voicename+"/espeak-ng-data");
             config.model.vits.model         = onnx.c_str();
-            config.model.vits.tokens        = (models_path_+"/vits-piper-"+voicename+"/tokens.txt").c_str();
-            config.model.vits.data_dir      = (models_path_+"/vits-piper-"+voicename+"/espeak-ng-data").c_str();
+            config.model.vits.tokens        = tokens.c_str();
+            config.model.vits.data_dir      = espeak.c_str();
             config.model.vits.noise_scale   = 0.667f; // Controla la expresividad/varianza
             config.model.vits.noise_scale_w = 0.8f;   // Varianza en la duración de los fonemas
             config.model.vits.length_scale  = 1.0f;   // 1.0 = normal, >1.0 más lento, <1.0 más rápido
-            config.model.num_threads = (num_threads_>MAX_THREADS_) ? MAX_THREADS_ : num_threads_;
-            config.model.debug = 1;         // 1 para logs en consola
-            std::cout << "[TTSMgr]  Initializating offline tts" << std::endl;
+            config.model.num_threads = num_threads_;
+            config.model.debug = 0;         // 1 para logs en consola
+            std::cout << "[TTSMgr]  Initializating voice " << voicename << std::endl;
             
             const SherpaOnnxOfflineTts* tts_model = SherpaOnnxCreateOfflineTts(&config);
 
             if (!tts_model) {
-                std::string err = "[TTSMgr]  ERROR Cannot load voice model: " + models_path_+"/vits-piper-"+voicename;
+                std::string err = "[TTSMgr]  ERROR Cannot load voice model: " + onnx;
                 std::cerr << err << std::endl;
                 #ifdef _WIN32
                     MessageBoxA(NULL, err.c_str(), "ERROR", MB_ICONERROR | MB_OK);
@@ -65,8 +67,6 @@ public:
             tts_models_[voicename] = tts_model;
             return true;
         };
-
-        bool result;
 
         if(!load_vits_model("en_GB-alan-low")) 
             return false;
@@ -87,28 +87,42 @@ public:
         return true;
     }
 
-    void generate(std::string text, std::string wavname){
+    bool generate(std::string text, std::string wavname){
         int sid = 0; // speaker id
 
         std::cout << "[TTSMgr]  Generating audio" << std::endl;
-        const SherpaOnnxGeneratedAudio* audio = SherpaOnnxOfflineTtsGenerate(tts_models_["en-GB-alba-medium"], text.c_str(), sid, 1.0);
+        const SherpaOnnxGeneratedAudio* audio = SherpaOnnxOfflineTtsGenerate(tts_models_["en_US-kathleen-low"], text.c_str(), sid, 1.0);
+
+        if (!audio) {
+            std::string err = "[TTSMgr]  ERROR Cannot generate audio: " + wavname;
+            std::cerr << err << std::endl;
+            #ifdef _WIN32
+                MessageBoxA(NULL, err.c_str(), "ERROR", MB_ICONERROR | MB_OK);
+            #else
+                std::string comando = "zenity --error --title=\"TTS ERROR\" --text=\"" + msg + "\" 2>/dev/null";
+                system(comando.c_str());
+            #endif
+            return false;
+        }
 
         std::cout << "[TTSMgr]  Writing to file..." << std::endl;
-        SherpaOnnxWriteWave(audio->samples, audio->n, audio->sample_rate, "./test.wav");
+        SherpaOnnxWriteWave(audio->samples, audio->n, audio->sample_rate, (wavname+".wav").c_str());
 
         // You need to free the pointers to avoid memory leak in your app
         std::cout << "[TTSMgr]  Freeing memory" << std::endl;
         SherpaOnnxDestroyOfflineTtsGeneratedAudio(audio);
+
+        std::cout << "[TTSMgr]  Audio generated to" << wavname << ".wav" << std::endl; 
         
-        printf("Saved to ./test.wav\n");
+        return true;
     };
 
 private:
     using TTSModelsMap = std::unordered_map<std::string, const SherpaOnnxOfflineTts*>;
 
-    TTSModelsMap    tts_models_;        // TTS configurado con una voz
-    int32_t         num_threads_;       // Número de hilos con los que se generarán los audios
+    TTSModelsMap        tts_models_;        // TTS configurado con una voz
+    int32_t             num_threads_;       // Número de hilos con los que se generarán los audios
+    short               init_percent;       // Porcentaje de inicialización (100 = full init)
     
-    const int32_t               MAX_THREADS_    = 8;            // Número máximo de hilos de cada modelo
-    std::string const           models_path_    = "./voices/";  // Ruta de carpetas donde residen los modelos
+    std::string const   models_path_    = "./voices/";  // Ruta de carpetas donde residen los modelos
 };
