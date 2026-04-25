0becb40f8469b3177bf7bc89c4f3ae4d18014a5c
Sergio <xergio.e@hotmail.com>
2026-04-25 03:20:49 +0200

    wip cyclone: integración binding c++

--- toolchains/lib-cyclonedds.cmake
+++ toolchains/lib-cyclonedds.cmake
@@ -22,9 +22,13 @@ endif()
 # =======================
 set(ENABLE_SSL     OFF CACHE INTERNAL "")
 set(ENABLE_SHM     OFF CACHE INTERNAL "")
+set(ENABLE_SECURITY OFF CACHE INTERNAL "")
 set(BUILD_EXAMPLES OFF CACHE INTERNAL "")
 set(BUILD_TESTING  OFF CACHE INTERNAL "")
 set(BUILD_IDLC     ON  CACHE INTERNAL "")  # ON: necesario para compilar archivos .idl
+set(INSTALL_C_HEADERS OFF CACHE INTERNAL "")
+set(CYCLONEDDS_INSTALL_C_HEADERS OFF CACHE INTERNAL "")
+set(BUILD_CPP_BINDINGS ON CACHE INTERNAL "")
 
 # =======================
 # Descarga
@@ -37,6 +41,42 @@ FetchContent_Declare(
     SOURCE_DIR     "${CYCLONE_SRC_DIR}"
 )
 FetchContent_MakeAvailable(cyclonedds)
+
+# =======================
+# EL TRUCO DE LA DOCUMENTACIÓN
+# =======================
+# Aquí emulamos el CMAKE_PREFIX_PATH que pide la documentación.
+# Le decimos al paquete de C++ dónde están los archivos de configuración
+# que el paquete de C acaba de generar en tu carpeta _build.
+set(CycloneDDS_DIR "${cyclonedds_BINARY_DIR}/lib/cmake/CycloneDDS" CACHE PATH "" FORCE)
+
+message(STATUS "Fetching CycloneDDS C++ Bindings...")
+
+# =======================
+# Opciones C++ Binding
+# =======================
+# La documentación menciona estas opciones, las desactivamos para compilar más rápido
+set(BUILD_TESTING OFF CACHE INTERNAL "")
+set(BUILD_EXAMPLES OFF CACHE INTERNAL "")
+
+# Descarga y compilación del Binding C++
+FetchContent_Declare(
+    cyclonedds_cxx
+    GIT_REPOSITORY https://github.com/eclipse-cyclonedds/cyclonedds-cxx.git
+    GIT_TAG        0.10.5  # Debe coincidir exactamente con el Core
+    GIT_SHALLOW    TRUE
+)
+FetchContent_MakeAvailable(cyclonedds_cxx)
+
+
+# Omitir warnings de la propia librería
+target_compile_options(cyclonedds PRIVATE
+    $<$<NOT:$<CXX_COMPILER_ID:MSVC>>:
+      -Wno-shadow
+      -Wno-enum-enum-conversion
+      -Wno-unused-includes
+    >
+)
 
 # =======================
 # Compilación de archivos IDL
