1e355dfb460af0ed3ee19062de9580bb03f5fee3
Sergio <xergio.e@hotmail.com>
2026-04-28 02:36:42 +0200

    sys: + comentarios doxy

--- include/system/sys.hpp
+++ include/system/sys.hpp
@@ -3,17 +3,52 @@
 #include <string>
 #include "system/LogMgr.hpp"
 
+
+// Macros simplificados para escribir mensajes:
 #define SYS_ERROR(module, msg) Sys::instance().error(module, msg)
 #define SYS_WARN(module, msg)  Sys::instance().warning(module, msg)
 #define SYS_INFO(module, msg)  Sys::instance().info(module, msg)
 
-// singleton
+
+/**
+ * @class Sys
+ * @brief Clase Singleton, para obtener/modificar configuraciones de aplicación
+ *      escribir en registro, etc..
+ */
 class Sys {
+
 public:
+
+// General ------------------------------------------------------------------------------
+
+    /**
+     * @brief Declara la instancia estática de la clase
+     */
     static Sys& instance();
 
+
+// Log ----------------------------------------------------------------------------------
+
+    /**
+     * @brief Genera un mensaje de error en cout, log.
+     * @warning Bloqueante, con pop-up en windows y linux (con zenity)
+     * @param module Módulo (Clase) desde donde se genera el error
+     * @param msg Mensaje de error
+     */
     void error(std::string const& module, std::string const& msg);
+
+    /**
+     * @brief Genera un mensaje de warning en cout, log.
+     * @param module Módulo (Clase) desde donde se genera el error
+     * @param msg Mensaje de error
+     */
     void warning(std::string const& module, std::string const& msg);
+
+    /**
+     * @brief Genera un mensaje de información en cout, log.
+     * @param module Módulo (Clase) desde donde se genera el error
+     * @param msg Mensaje de error
+     */
     void info(std::string const& module, std::string const& msg);
 
 
@@ -30,15 +65,35 @@ public:
     inline std::string wstringToString(std::wstring const& ws);
 
 private:
-    Sys(); // constructor privado
+
+// General ------------------------------------------------------------------------------
+
+    /**
+     * @brief Constructor privado en singleton
+     */
+    Sys();
+
+    /**
+     * @brief Destructor privado en singleton 
+     */
     ~Sys();
 
-    // Evitar copias
-    Sys(const Sys&) = delete;
-    Sys& operator=(const Sys&) = delete;
+    /**
+     * @brief Evitar copias
+     */
+    Sys(const Sys&)             = delete;
+    Sys& operator=(const Sys&)  = delete;
 
+
+// Pop-ups ------------------------------------------------------------------------------
+
+    /** 
+     * @brief Muestra una ventana de error según el SO
+     */
     void showPopup(const std::string& msg, const std::string& title);
 
-private:
-    LogMgr log_;
+
+/************ Variables ****************************************************************/
+
+    LogMgr log_;    // Clase para gestión de archivo de log interno
 };
