4c5c0c886cefdd00f18de3b20c72680ad9525a5d
Sergio <xergio.e@hotmail.com>
2026-04-30 23:58:25 +0200

    fastdds: forma preliminar para fetchcontent, configura bien

--- toolchains/lib-fastdds.cmake
+++ toolchains/lib-fastdds.cmake
@@ -8,7 +8,6 @@ set(COMPILE_EXAMPLES    OFF CACHE BOOL "" FORCE)
 set(BUILD_SHARED_LIBS   OFF CACHE BOOL "" FORCE)
 set(CHECK_DOCUMENTATION OFF CACHE BOOL "" FORCE)
 set(COMPILE_EXAMPLES    OFF CACHE BOOL "" FORCE)
-set(BUILD_SHARED_LIBS   OFF CACHE BOOL "" FORCE)
 set(CHECK_DOCUMENTATION OFF CACHE BOOL "" FORCE)
 set(STRICT_REALTIME     OFF CACHE BOOL "" FORCE)
 set(FOONATHAN_MEMORY_BUILD_EXAMPLES OFF CACHE BOOL "" FORCE)
@@ -22,43 +21,55 @@ set(FASTRTPS_INSTALLER_LOC "${CMAKE_BINARY_DIR}/install" CACHE PATH "" FORCE)   
 # Lógica de descarga
 # =======================
 
-# --------------------------
-function(fetch_lib LIB URL TAG)
-    message(STATUS "Fetching ${LIB} library...")
-    string(TOLOWER ${LIB} LIB_LOW)
-    string(TOUPPER ${LIB} LIB_UP)
-    set(SRC_PATH "${EXTERNAL_LIB_PATH}/${LIB_LOW}_src")
-    # Usa la librería ya descargada en external/ si existe
-    if (EXISTS "${EXTERNAL_LIB_PATH}/${LIB_LOW}_src/.git")
-    message(STATUS "Using local ${LIB} source")
-    set(FETCHCONTENT_SOURCE_DIR_${LIB_UP}
-        ${SRC_PATH}
-        CACHE PATH "" FORCE)
-    endif()
-    message(STATUS "URL: " ${URL})
-    message(STATUS "TAG: " ${TAG})
-    message(STATUS "SRC_PATH: " ${SRC_PATH})
-    FetchContent_Declare(
-        ${LIB}
-        GIT_REPOSITORY ${URL}
-        GIT_TAG        ${TAG}  
-        SOURCE_DIR     ${SRC_PATH}
-        EXCLUDE_FROM_ALL TRUE
-    )
-    FetchContent_MakeAvailable(${LIB_LOW})
-endfunction()
-# --------------------------
 
-# 1. Dependencia: Fast-CDR (Obligatoria para Fast-DDS)
-fetch_lib("fastcdr"              "https://github.com/eProsima/Fast-CDR"     "v2.3.2")
-# 2. Dependencia: foonathan_memory (Gestión de memoria)
-fetch_lib("foonathan_memory"     "https://github.com/foonathan/memory"      "v0.7-3")
-# 3. FastDDS
-fetch_lib("fastdds"              "https://github.com/eProsima/Fast-DDS"     "v3.2.1")
+# =======================
+# Dependencia: FastCDR
+# =======================
 
+if (EXISTS "${EXTERNAL_LIB_PATH}/fastcdr_src/.git")
+  message(STATUS "Using local fastcdr source")
+  set(FETCHCONTENT_SOURCE_DIR_FASTCDR
+      "${EXTERNAL_LIB_PATH}/fastcdr_src"
+      CACHE PATH "" FORCE)
+endif()
+FetchContent_Declare(
+    fastcdr
+    GIT_REPOSITORY "https://github.com/eProsima/Fast-CDR"
+    GIT_TAG        v1.0.29
+    GIT_SHALLOW    TRUE        # habilita --depth 1
+    SOURCE_DIR     "${EXTERNAL_LIB_PATH}/fastcdr_src"
+    EXCLUDE_FROM_ALL TRUE
+)
+FetchContent_MakeAvailable(fastcdr)
 
-# =========================================================
-# NOTA IMPORTANTE: 
-# No necesitas add_library(fastdds STATIC IMPORTED) ni target_include_directories.
-# El target 'fastdds' ya es exportado automáticamente por eProsima.
-# =========================================================
+if (EXISTS "${EXTERNAL_LIB_PATH}/foomemory_src/.git")
+  message(STATUS "Using local foomemory source")
+  set(FETCHCONTENT_SOURCE_DIR_FOOMEMORY
+      "${EXTERNAL_LIB_PATH}/foomemory_src"
+      CACHE PATH "" FORCE)
+endif()
+FetchContent_Declare(
+    foomemory
+    GIT_REPOSITORY "https://github.com/foonathan/memory"
+    GIT_TAG        v0.7-3
+    GIT_SHALLOW    TRUE        # habilita --depth 1
+    SOURCE_DIR     "${EXTERNAL_LIB_PATH}/foomemory_src"
+    EXCLUDE_FROM_ALL TRUE
+)
+FetchContent_MakeAvailable(foomemory)
+
+if (EXISTS "${EXTERNAL_LIB_PATH}/fastdds_src/.git")
+  message(STATUS "Using local foomemory source")
+  set(FETCHCONTENT_SOURCE_DIR_FASTDDS
+      "${EXTERNAL_LIB_PATH}/fastdds_src"
+      CACHE PATH "" FORCE)
+endif()
+FetchContent_Declare(
+    foomemory
+    GIT_REPOSITORY "https://github.com/eProsima/Fast-DDS"
+    GIT_TAG        v3.2.1
+    GIT_SHALLOW    TRUE        # habilita --depth 1
+    SOURCE_DIR     "${EXTERNAL_LIB_PATH}/fastdds_src"
+    EXCLUDE_FROM_ALL TRUE
+)
+FetchContent_MakeAvailable(foomemory)
