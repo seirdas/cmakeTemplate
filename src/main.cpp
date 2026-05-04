242bb14a0ed7662319cdcf0909de074e24eaabd8
Sergio <secrespo@indra.es>
2026-05-04 20:40:16 +0200

    cyclone: implemento pequeña prueba temp en main

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,28 @@
 #include <filesystem>           // Controla directorios, rutas, etc.
 #include "AppController.hpp"    // Clase controladora de aplicación
 
+#include "dds.hpp"
+
 int main(int argc, char** argv) {
     if (argc > 0) std::filesystem::current_path(std::filesystem::path(argv[0]).parent_path());
+
+
+    /* Prueba cyclonedds */
+
+    // Leer el argumento de la consola para decidir qué rol tomar
+    std::string mode = "pub"; // Por defecto es publicador
+
+    if (argc > 1)
+        mode = argv[1];
+
+    if (mode == "sub")
+        run_subscriber();       // <- bloqueante
+    else if (mode == "pub")
+        run_publisher();        // <- bloqueante
+
+
+
+
 
     // Instancia controladora de la aplicación
     AppController App;
