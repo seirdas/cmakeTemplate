8a63d8fd35adf3254b0fbc69fcfbed06c71514d9
Sergio <xergio.e@hotmail.com>
2026-05-09 21:13:18 +0200

    main: auto por filepath

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 
 int main(int argc, char** argv) {
     // Asegurar directorio del exe (para archivos de entorno de desarrollo)
-    auto path = std::filesystem::absolute(argv[0]);
+    std::filesystem::path path = std::filesystem::absolute(argv[0]);
     if (std::filesystem::exists(path))
         std::filesystem::current_path(path.parent_path());
 
