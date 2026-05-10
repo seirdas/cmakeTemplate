44d49dfd1506ea63f620882ff643430ccf980054
Sergio <secrespo@indra.es>
2026-05-10 12:57:48 +0200

    json: añadidos identificadores de libreria en comentarios

--- toolchains/file/lib-json.cmake
+++ toolchains/file/lib-json.cmake
@@ -5,7 +5,7 @@
 include(FetchContent)
 cmake_policy(SET CMP0135 NEW) 
 
-message(STATUS "Fetching nlohmann/json library...")
+message(STATUS "[json] Fetching nlohmann/json library...")
 
 # Version de json
 set(JSON_VERSION "v3.12.0")
@@ -20,10 +20,10 @@ FetchContent_Declare(
 
 # Comprobar si ya existe localmente para evitar reconexiones innecesarias
 if(NOT EXISTS "${JSON_INSTALL_DIR}/include/nlohmann/json.hpp")
-    message(STATUS "nlohmann/json not found. Downloading version ${JSON_VERSION}...")
+    message(STATUS "[json] nlohmann/json not found. Downloading version ${JSON_VERSION}...")
     FetchContent_MakeAvailable(nlohmann_json)
 else()
-    message(STATUS "Using local nlohmann/json header from: ${JSON_INSTALL_DIR}")
+    message(STATUS "[json] Using local nlohmann/json header from: ${JSON_INSTALL_DIR}")
 endif()
 
 add_library(nlohmann_json INTERFACE)
