535d05924a0c1da79e866dd3dc505503f677c173
Sergio <secrespo@indra.es>
2026-06-23 17:25:15 +0200

    app: metodos IApp en IAppOverrides.cpp

--- src/app/AppController.cpp
+++ src/app/AppController.cpp
@@ -1,6 +1,5 @@
 #include "app/AppController.hpp"
 #include "system/SystemMgr.hpp"
-#include <chrono>               // Controla tiempos de espera
 
 // General ------------------------------------------------------------------------------
 
@@ -159,7 +158,6 @@ void AppController::TWorker() {
     SYS_INFO("TWorker", "Consumer thread stopped.");
 }
 
-
 void AppController::TPruebas() {
     SYS_INFO("TPruebas","init TPruebas");
 
@@ -175,120 +173,4 @@ void AppController::TPruebas() {
 }
 
 
-// IAppControl methods ------------------------------------------------------------
-    
-    /* 
-    * Aquí solo deberían ir acciones que se ejecuten sobre otros módulos
-    * Para la "solicitud" de datos, se debería hacer un patrón observador
-    */
-
-    // Aplicación -----------------------------------------------------------------------
-
-    std::string AppController::getVersion() const noexcept { 
-        return version_; 
-    }
-
-    void AppController::setOnlineMode(bool nuevo_online_mode) noexcept { 
-
-        std::unique_lock<std::mutex> lock(online_mtx_);
-        if(online_mode_==nuevo_online_mode) return;
-        online_mode_=nuevo_online_mode;
-        lock.unlock();
-
-        if(online_mode_) {
-            net_.start();
-            online_cv_.notify_all();
-        }
-        else
-            net_.stop();
-    };
-
-    bool AppController::isOnlineMode() const noexcept {
-        return online_mode_;
-    };
-
-    
-    // Sockets --------------------------------------------------------------------------
-    
-    bool AppController::addReceiver() const noexcept {
-        // TODO
-        return false;
-    }
-
-    bool AppController::removeReceiver() const noexcept {
-        // TODO
-        return false;
-    }
-
-
-    // Audio ----------------------------------------------------------------------------
-
-    std::vector<std::string> AppController::getAvailableInputDevices() noexcept {
-        return snd_.getAvailableInputs();
-    }
-
-    std::vector<std::string> AppController::getAvailablePlaybackDevices() noexcept {
-        return snd_.getAvailablePlaybacks();
-    }
-
-    void AppController::refreshAudioDevices() noexcept {
-        snd_.updateDevices();
-        return;
-    }
-
-    bool AppController::addInputDevice(std::string const& name) noexcept {
-        return snd_.addCaptureDevice(name, 0);
-    }
-
-    bool AppController::removeInputDevice(unsigned short index) noexcept {
-        return snd_.removeInputDevice(index);
-    }
-
-    bool AppController::StartRecording(unsigned short index) noexcept{
-        return snd_.startRec_snd(index); 
-    }
-    
-    bool AppController::StopRecording(unsigned short index) noexcept{
-        return snd_.stopRec_snd(index); 
-    }
-
-    size_t AppController::getInputBufferSize(unsigned short index) noexcept {
-        return snd_.getInputBufferSize(index); 
-    }
-
-    size_t AppController::getInputRecBufferSize(unsigned short index) noexcept {
-        return snd_.getInputRecBufferSize(index); 
-    }
-
-    bool AppController::isInputDeviceValid(unsigned short index) noexcept {
-        return snd_.isInputDeviceValid(index);
-    }
-
-
-
-
-    // TTS ----------------------------------------------------------------------------
-    
-    bool AppController::TTSgenerate(
-        std::string const& modelName, 
-        std::string const& text, 
-        std::string const& wavname)
-        noexcept {
-            return tts_.generate(modelName, text, wavname);
-        };
-
-    TTSData AppController::getTTSData() noexcept {
-        TTSData data;
-
-        data.available_models     = tts_.getAvailableModels();
-        data.loaded_models        = tts_.getLoadedModels();
-        data.num_available_models = tts_.numAvailableModels();
-        data.num_loaded_models    = tts_.numLoadedModels();
-        data.init_percent         = static_cast<short>(100.0 * data.num_loaded_models / data.num_available_models);
-        
-        return data;
-    }
-
-    std::string AppController::getTTSProcessingText(std::string modelName) const noexcept {
-        return tts_.getProccesingText(modelName);
-    }
+// IAppControl methods en IAppOverrides.cpp
---
+++ src/app/IAppOverrides.cpp
@@ -0,0 +1,119 @@
+#include "app/AppController.hpp"
+
+/* 
+* Aquí solo deberían ir acciones que se ejecuten sobre otros módulos
+* Para la "solicitud" de datos, se debería hacer un patrón observador
+*/
+
+
+/* --- IAppControl methods --- */
+
+
+// Aplicación -----------------------------------------------------------------------
+
+std::string AppController::getVersion() const noexcept { 
+    return version_; 
+}
+
+void AppController::setOnlineMode(bool nuevo_online_mode) noexcept { 
+
+    std::unique_lock<std::mutex> lock(online_mtx_);
+    if(online_mode_==nuevo_online_mode) return;
+    online_mode_=nuevo_online_mode;
+    lock.unlock();
+
+    if(online_mode_) {
+        net_.start();
+        online_cv_.notify_all();
+    }
+    else
+        net_.stop();
+};
+
+bool AppController::isOnlineMode() const noexcept {
+    return online_mode_;
+};
+
+
+// Sockets --------------------------------------------------------------------------
+
+bool AppController::addReceiver() const noexcept {
+    // TODO
+    return false;
+}
+
+bool AppController::removeReceiver() const noexcept {
+    // TODO
+    return false;
+}
+
+
+// Audio ----------------------------------------------------------------------------
+
+std::vector<std::string> AppController::getAvailableInputDevices() noexcept {
+    return snd_.getAvailableInputs();
+}
+
+std::vector<std::string> AppController::getAvailablePlaybackDevices() noexcept {
+    return snd_.getAvailablePlaybacks();
+}
+
+void AppController::refreshAudioDevices() noexcept {
+    snd_.updateDevices();
+    return;
+}
+
+bool AppController::addInputDevice(std::string const& name) noexcept {
+    return snd_.addCaptureDevice(name, 0);
+}
+
+bool AppController::removeInputDevice(unsigned short index) noexcept {
+    return snd_.removeInputDevice(index);
+}
+
+bool AppController::StartRecording(unsigned short index) noexcept{
+    return snd_.startRec_snd(index); 
+}
+
+bool AppController::StopRecording(unsigned short index) noexcept{
+    return snd_.stopRec_snd(index); 
+}
+
+size_t AppController::getInputBufferSize(unsigned short index) noexcept {
+    return snd_.getInputBufferSize(index); 
+}
+
+size_t AppController::getInputRecBufferSize(unsigned short index) noexcept {
+    return snd_.getInputRecBufferSize(index); 
+}
+
+bool AppController::isInputDeviceValid(unsigned short index) noexcept {
+    return snd_.isInputDeviceValid(index);
+}
+
+
+// TTS ----------------------------------------------------------------------------
+
+bool AppController::TTSgenerate(
+    std::string const& modelName, 
+    std::string const& text, 
+    std::string const& wavname)
+    noexcept {
+        return tts_.generate(modelName, text, wavname);
+    };
+
+TTSData AppController::getTTSData() noexcept {
+    TTSData data;
+
+    data.available_models     = tts_.getAvailableModels();
+    data.loaded_models        = tts_.getLoadedModels();
+    data.num_available_models = tts_.numAvailableModels();
+    data.num_loaded_models    = tts_.numLoadedModels();
+    data.init_percent         = static_cast<short>(100.0 * data.num_loaded_models / data.num_available_models);
+    
+    return data;
+}
+
+std::string AppController::getTTSProcessingText(std::string modelName) const noexcept {
+    return tts_.getProccesingText(modelName);
+}
