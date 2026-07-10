8647ae3cfcd91f8679395121ec22a6a49ae38e9e
Sergio <secrespo@indra.es>
2026-07-10 12:58:42 +0200

    tts: ttsplayer con funciones miembro para inyectar para "generar" y "reproducir" audio tts

--- include/tts/TTSPlayer.hpp
+++ include/tts/TTSPlayer.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <string>
+#include <functional>
+#include "system/SystemMgr.hpp"
 
 class TTSCore;
 
@@ -13,23 +15,99 @@ public:
     /**
      * @brief Constructor 
      */
-    TTSPlayer(TTSCore& ttsCore, std::string AIM_name);
+    TTSPlayer() :
+        fn_textToAudio(nullptr),
+        fn_audioToPlayback(nullptr),
+        active_tasks_(0)
+    {
+
+    }
     
     /**
      * @brief Destructor 
      */
-    ~TTSPlayer();
+    ~TTSPlayer() {
+
+    }
 
 
 // Ejecución ----------------------------------------------------------------------------
 
-    bool init(void* config);
+    bool init(std::string const& pbName) {
+        playbackName_ = pbName;
+    }
 
-    void loadConfig(void* config);
+    bool play(std::string const& modelName, std::string const& text) {
+        if (!fn_textToAudio || !fn_audioToPlayback) {
+            SYS_WARN("TTSPlayer","Cannot generate audio: Callback functions don't exist.");
+            return false;
+        }
+
+        active_tasks_++;
+
+        // Generar audio usando función inyectada de tts
+        std::vector<float> audio = fn_textToAudio(modelName, text);
+
+        // Reproducir audio por el playback
+        fn_audioToPlayback(audio, playbackName_);
+
+        // #TODO notificar que ha terminado de reproducir
+
+        active_tasks_--;
+    }
+
+// Datos --------------------------------------------------------------------------------
+
+    void setPlaybackDev(std::string const& pbName) {
+        playbackName_ = pbName;
+    }
+
+// Inyección de funciones ---------------------------------------------------------------
+
+    /**
+     * @brief Función inyectada que utiliza el núcleo de tts para pasar un texto a audio
+     * @param modelName Nombre del modelo tts a usar
+     * @param text Texto para convertir a audio
+     * @returns std::vector<float> Vector de muestras de audio en formato float
+     */
+    using TTSFunction = std::function<std::vector<float>(std::string const& modelName, std::string const& text)>;
+
+    void setTTSCallback(TTSFunction fn) {
+        fn_textToAudio = std::move(fn); 
+    }
+
+    /**
+     * @brief Función inyectada que utiliza el soundMgr para reproducir un audio
+     * @param audio Audio a reproducir (obtenido del tts)
+     * @param playbackName Nombre del dispositivo playback por el que reproducir el audio
+     */
+    using PlaybackFunction = std::function<void(std::vector<float> audio, std::string const& playbackName)>;
+
+
+    void setPlaybackCallback(PlaybackFunction fn) {
+        fn_audioToPlayback = std::move(fn);
+    }
+
+
+
+    bool isBusy() {
+        return (active_tasks_ > 0);
+    }
 
 
 private:
 
-    bool                is_busy_;
+/************ Variables ********************************************************/
 
+    // Funciones inyectadas
+    TTSFunction       fn_textToAudio;       ///< Función inyectada para pasar de texto a audio
+    PlaybackFunction  fn_audioToPlayback;   ///< Función inyectada para reproducir audio en playback
+
+    // Ejecución 
+    std::atomic<short>  active_tasks_;      ///< Indica si hay algo en ejecución
+
+    // Datos
+    std::string         playbackName_;      ///< Nombre del playback por el que se reproduce
+
+    // Datos temporales
 };
