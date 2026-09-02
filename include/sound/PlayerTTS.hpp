891727899a2df0c97d2095552af5c53a6abc3c57
Sergio <secrespo@indra.es>
2026-09-02 02:29:48 +0200

    (WIP) borro copia duplicada

--- include/sound/PlayerTTS.hpp
+++ include/sound/PlayerTTS.hpp
@@ -54,14 +54,6 @@ public:
      */
     bool close() override;
 
-    // Deshabilitar copia explícitamente (elimina warnings C4625 y C4626)
-    PlayerTTS(PlayerTTS const&) = delete;
-    PlayerTTS& operator=(PlayerTTS const&) = delete;
-
-    // (Opcional) Si necesitas mover la instancia, habilita o elimina el movimiento:
-    PlayerTTS(PlayerTTS&&) = delete;
-    PlayerTTS& operator=(PlayerTTS&&) = delete;
-
 
 // Ejecución ----------------------------------------------------------------------------
 
